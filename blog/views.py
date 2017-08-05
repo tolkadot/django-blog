d5e2e71a282c756f6c8561cf4cfde0b5ae3005bd
tolkadot <tolkadot@gmail.com>
2017-08-05 13:34:57 +1000

    single post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 #from django.contrib.auth.models import User
 from .models import Post
@@ -7,6 +7,10 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.filter(author=User.objects.get(username='tolkadot'))
-    return render(request, 'blog/post_list.html', {'blog' : posts})
+    return render(request, 'blog/post_list.html', {'posts' : post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
