de1fdd26755f2cb725e4fbaec50ec899e1bce4ef
tolkadot <tolkadot@gmail.com>
2017-08-05 11:51:27 +1000

    first page up

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+#from django.contrib.auth.models import User
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.filter(author=User.objects.get(username='tolkadot'))
+    return render(request, 'blog/post_list.html', {'blog' : posts})
